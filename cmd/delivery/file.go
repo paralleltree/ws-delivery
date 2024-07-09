1aa23673676cc69305cdc1aae084b204d32b2208
Paralleltree <paralleltree@outlook.com>
2024-07-09 13:41:07 +0900

    Implement file tailer

---
+++ cmd/delivery/file.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/hpcloud/tail"
+)
+
+func tailLog(ctx context.Context, path string) <-chan Message[string] {
+	ch := make(chan Message[string])
+	go func() {
+		f, err := tail.TailFile(path, tail.Config{Follow: true, Location: &tail.SeekInfo{Offset: 0, Whence: io.SeekEnd}})
+		if err != nil {
+			ch <- Message[string]{Err: fmt.Errorf("tail file: %w", err)}
+			return
+		}
+		defer f.Cleanup()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			case v := <-f.Lines:
+				if v == nil {
+					ch <- Message[string]{Err: fmt.Errorf("tail: got nil line. waiting 1 second")}
+					select {
+					case <-ctx.Done():
+					case <-time.After(time.Second):
+					}
+					continue
+				}
+				if v.Err != nil {
+					ch <- Message[string]{Err: fmt.Errorf("read lines: %w", err)}
+					continue
+				}
+				ch <- Message[string]{Body: v.Text}
+			}
+		}
+	}()
+	return ch
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/paralleltree/ws-delivery
 go 1.22.2
 
 require golang.org/x/net v0.27.0
+
+require (
+	github.com/hpcloud/tail v1.0.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
